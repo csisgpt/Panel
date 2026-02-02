5d8f0a0924c8b5821e20cb4bd492b14b30314745
roayaii <mohsenroayaei78@gmail.com>
2026-02-02 16:37:39 +0330

    fix

--- lib/api/config.ts
+++ lib/api/config.ts
@@ -1,7 +1,7 @@
 export type ApiMode = "mock" | "http";
 
-export const API_MODE: ApiMode =
-  process.env.NEXT_PUBLIC_API_MODE === "http" ? "http" : "mock";
+export const API_MODE: ApiMode = "http"
+  // process.env.NEXT_PUBLIC_API_MODE === "http" ? "http" : "mock";
 
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:3000";
 
